9848e88277cca45ca052647728cfc0ef647d2cf9
Pieter Nauwelaerts <pnauwela@ucsc.edu>
2024-03-29 22:18:24 -0700

    removing outdated  code

--- KRS-3301/src/variables.py
+++
@@ -1,9 +0,0 @@
-"""
-The variables file is for data that will be used throughout the project.  This could be static or
-dynamic data.  After being initial loaded by main.py, it can be imported and used in any module
-throughout the system.
-"""
-prog_val = -18 
-mic_val = -18
-
-#TODO - CHeck if you can put the passcode in here and not need to recertify 
