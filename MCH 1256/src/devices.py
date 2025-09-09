bf2cd2b9385eb052d86259c4b7537a2dfb90ef55
Scott makson <smakson@ucsc.edu>
2025-09-09 13:48:24 -0700

    Updated McH1256 IPs

--- MCH 1256/src/devices.py
+++ MCH 1256/src/devices.py
@@ -34,15 +34,15 @@ dvNBPB = UIDevice('PanelB')
 # passwordFile = File('user/password.txt', 'r')
 # password = str(passwordFile.readline())
 
-dvSW = Scaler.SSHClass('128.114.162.3', 22023, Credentials=('admin', '100%Becknerized'), Model='DTP3 IN2004 DI/DO') #password
+dvSW = Scaler.SSHClass('128.114.163.20', 22023, Credentials=('admin', '100%Becknerized'), Model='DTP3 IN2004 DI/DO') #password
 dvSW = GetConnectionHandler(dvSW, 'Temperature', pollFrequency=30)         
 
-dvPRJA = GetConnectionHandler(PRJ.SerialOverEthernetClass('128.114.162.4', 2003, Model='EB-690U'), 'LampUsage', pollFrequency=30)
+dvPRJA = GetConnectionHandler(PRJ.SerialOverEthernetClass('128.114.163.20', 2003, Model='EB-690U'), 'LampUsage', pollFrequency=30)
 dvPRJB = GetConnectionHandler(PRJ.SerialClass(dvIPCP, 'COM1', Model='EB-690U'), 'LampUsage', pollFrequency=30)
 
-dvCynap = Cynap.EthernetClass('128.114.162.0', 50915,  Credentials=('admin', '100%Becknerized'), Model='Cynap Pure Pro')    
+dvCynap = Cynap.EthernetClass('128.114.163.198', 50915,  Credentials=('admin', '100%Becknerized'), Model='Cynap Pure Pro')    
 
-dvBiamp = Biamp.SSHClass('128.114.162.0', 22, Credentials=('admin', '100%Becknerized'), Model='TesiraFORTE DAN AI') #password
+dvBiamp = Biamp.SSHClass('128.114.163.199', 22, Credentials=('admin', '100%Becknerized'), Model='TesiraFORTE DAN AI') #password
 
 IPCP_ID = 'IPCP'; NBPA_ID = 'NBPA'; NBPB_ID = 'NBPB'; SW_ID = 'SW'; PRJA_ID = 'PRJA'; PRJB_ID = 'PRJB'; CY_ID = 'Cynap'; BMP_ID = 'Biamp'
 
