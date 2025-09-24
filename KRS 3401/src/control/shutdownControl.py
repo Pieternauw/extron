eef272be881656426299f05d675af19195b5b212
Pieter Nauwelaerts <pnauwela@ucsc.edu>
2025-09-24 10:40:06 -0700

    typo fixed

--- KRS 3401/src/control/shutdownControl.py
+++ KRS 3401/src/control/shutdownControl.py
@@ -47,7 +47,7 @@ def Shutdown():
     dvPRJFront.SetPower('Off', None)
     PRJTimer.Restart()
         
-    dvCynap.Set('EndPresentation', None, {'Deete Recordings Folder': 'Yes', 'Delete Snapshots Folder': 'Yes', 'Power Off Mode': 'New Presentation'})
+    dvCynap.Set('EndPresentation', None, {'Delete Recordings Folder': 'Yes', 'Delete Snapshots Folder': 'Yes', 'Power Off Mode': 'New Presentation'})
         
     tlp.input_set.SetCurrent(None)
     dvScalar.SetInput('3', {'Type': 'Audio/Video'})
