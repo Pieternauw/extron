3457adf269e4f0a55728dc97cfa6daafd0050ec8
Pieter Nauwelaerts <pnauwela@ucsc.edu>
2025-09-24 12:35:54 -0700

    button definition

--- KRS 3201/src/ui/tlpSourceSelect.py
+++ KRS 3201/src/ui/tlpSourceSelect.py
@@ -178,4 +178,4 @@ def SwitchInput(button:Button, state):
         btn_leftSourceSound.SetVisible(True)
         btn_rightSourceSound.SetVisible(False)
         
-btn_wirelessDisconnect = Button(dvTLPMain, 18)
+btn_wirelessDisconnect = Button(dvTLPMain, 64)
